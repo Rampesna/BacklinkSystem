44ecafb8c9a9353d263cf9a226802bf94ea12f44
Talha Can <rampesna@gmail.com>
2020-01-20 17:54:31 +0300

    20-01-2020 17:54 Talha Can
    Theme Fixed

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -6,22 +6,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="icon" href="{{ asset('assets/images/favicon.ico') }}" type="image/x-icon"> <!-- Favicon-->
-    <title>@yield('title') - {{ config('app.name') }}</title>
-    <meta name="description" content="@yield('meta_description', config('app.name'))">
-    <meta name="author" content="@yield('meta_author', config('app.name'))">
-    @yield('meta')
-
-    @stack('before-styles')
+    <title>@yield('site_title')</title>
+    <meta name="description" content="@yield('meta_description')">
+    <meta name="keywords" content="@yield('meta_keywords')">
+    <meta name="author" content="@yield('meta_author')">
 
     <link rel="stylesheet" href="{{ asset('assets/vendor/bootstrap/css/bootstrap.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/vendor/font-awesome/css/font-awesome.min.css') }}">    
-    <link rel="stylesheet" href="{{ asset('assets/vendor/animate-css/vivify.min.css') }}">    
+    <link rel="stylesheet" href="{{ asset('assets/vendor/font-awesome/css/font-awesome.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/vendor/animate-css/vivify.min.css') }}">
 
-    @stack('after-styles')    
-    @if (trim($__env->yieldContent('page-styles')))    
-        @yield('page-styles')
-    @endif    
-    <!-- Custom Css -->
+    @yield('custom-header')
+
     <link rel="stylesheet" href="{{ asset('assets/css/site.min.css') }}">
 </head>
 
@@ -38,7 +33,7 @@
     </div>
 </div>
 
-@include('layout.themesetting')
+{{--@include('layout.themesetting')--}}
 
 <!-- Overlay For Sidebars -->
 <div class="overlay"></div>
@@ -46,9 +41,9 @@
 <div id="wrapper">
 
     @include('layout.navbar')
-    @include('layout.megamenu')
+{{--    @include('layout.megamenu')--}}
     @include('layout.searchbar')
-    @include('layout.rightbar')
+{{--    @include('layout.rightbar')--}}
     @include('layout.sidebar')
 
     <div id="main-content">
@@ -56,38 +51,16 @@
             <div class="block-header">
                 <div class="row clearfix">
                     <div class="col-md-6 col-sm-12">
-                        <h1>@yield('title')</h1>
+                        <h1></h1>
                         <nav aria-label="breadcrumb">
                             <ol class="breadcrumb">
-                                <li class="breadcrumb-item"><a href="{{route('mypage.index')}}"><i class="icon-home"></i></a></li>
-                                @if (trim($__env->yieldContent('parentPageTitle')))
-                                    <li class="breadcrumb-item">@yield('parentPageTitle')</li>
-                                @endif
-                                @if (trim($__env->yieldContent('title2')))
-                                    <li class="breadcrumb-item">@yield('title2')</li>
-                                @endif
-                                @if (trim($__env->yieldContent('title')))
-                                    <li class="breadcrumb-item active">@yield('title')</li>
-                                @endif
+
                             </ol>
                         </nav>
-                    </div>            
+                    </div>
                     <div class="col-md-6 col-sm-12 text-right hidden-xs">
                         <div class="d-flex flex-row-reverse">
-                            <a href="https://themeforest.net/user/puffintheme/portfolio" class="btn btn-success ml-1 mr-1" ><i class="icon-basket"></i> Buy Now</a>
-                            <a href="javascript:void(0);" class="btn btn-primary" >Go</a>
-                            <select class="custom-select w100 mr-3">
-                                <option selected="">USA</option>
-                                <option value="2">India</option>
-                                <option value="3">Australia</option>
-                            </select>
-                            <select class="custom-select w150 mr-3">                                
-                                <option value="2">Last 3 Days</option>
-                                <option selected="">Last 7 Days</option>
-                                <option value="2">Last 15 Days</option>
-                                <option value="2">Last 1 Month</option>
-                                <option value="2">Last 1 Year</option>
-                            </select>
+
                         </div>
                     </div>
                 </div>
@@ -99,9 +72,11 @@
     </div>
 </div>
 
+
+@yield('custom-footer')
 <!-- Scripts -->
 @stack('before-scripts')
-<script src="{{ asset('assets/bundles/libscripts.bundle.js') }}"></script>    
+<script src="{{ asset('assets/bundles/libscripts.bundle.js') }}"></script>
 <script src="{{ asset('assets/bundles/vendorscripts.bundle.js') }}"></script>
 
 @stack('after-scripts')
